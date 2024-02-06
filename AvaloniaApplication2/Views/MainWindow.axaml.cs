36a43e11b160f9de2c244156b4909895f611b557
Larcade <cepbora@vk.com>
2024-02-06 11:48:24 +0300

    qwe

--- AvaloniaApplication2/Views/MainWindow.axaml.cs
+++ AvaloniaApplication2/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace AvaloniaApplication2.Views
@@ -13,25 +14,39 @@ namespace AvaloniaApplication2.Views
         public MainWindow()
         {
             InitializeComponent();
+            KeyUp += MainWindow_KeyDown;
 
             _firstNameTextBox = this.FindControl<TextBox>("first_name");
             _lastNameTextBox = this.FindControl<TextBox>("last_name");
             _listBox = this.FindControl<ListBox>("listbox");
+            
         }
         
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter )
+            {
+                if (first_name.Text != null && last_name.Text != null && 
+                    first_name.Text != "" && last_name.Text != "")
+                {
+                    AddToListBox_Click(sender, e);
+                }
+                
+            }
+        }
 
         private void AddToListBox_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             string firstName = _firstNameTextBox.Text;
             string lastName = _lastNameTextBox.Text;
-
             string fullName = $"{firstName} {lastName}";
 
-            if (firstName != "" && lastName != "")
+            if (firstName != null && lastName != null && 
+                firstName != "" && lastName != "")
             {
                 (DataContext as MainWindowViewModel)?.ListItems.Add(fullName);
             }
-                
+            
             first_name.Text = "";
             last_name.Text = "";
 
--- AvaloniaApplication2/obj/Debug/net8.0/AvaloniaApplication2.AssemblyInfo.cs
+++ AvaloniaApplication2/obj/Debug/net8.0/AvaloniaApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c03ceea5307dc86501d56b4f2f5c68c95525a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6077d4f80feed8e9716f5c1e3266aa7cfd8ed3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
