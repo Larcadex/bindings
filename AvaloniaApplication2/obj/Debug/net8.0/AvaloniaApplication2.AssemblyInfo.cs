12f54693b1282924c0ff101d35289ae6a0d81ecc
Larcade <cepbora@vk.com>
2024-02-08 12:58:07 +0300

    last

--- AvaloniaApplication2/obj/Debug/net8.0/AvaloniaApplication2.AssemblyInfo.cs
+++ AvaloniaApplication2/obj/Debug/net8.0/AvaloniaApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6077d4f80feed8e9716f5c1e3266aa7cfd8ed3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53082d15df00f9ed652754d46b15d07b65d31473")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
